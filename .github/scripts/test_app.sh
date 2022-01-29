165ed4be571366c18b28587b195d859bbac56d25
marty <marty@martyulrich.com>
2022-01-28 15:56:04 -1000

    test

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-sudo xcodebuild -project iosApp.xcodeproj \
+xcodebuild -project iosApp.xcodeproj \
             -scheme iosApp \
             -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
             clean test | xcpretty
