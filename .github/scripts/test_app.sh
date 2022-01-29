d129dd36fa367b48fd0face979cfa818b962735b
marty <marty@martyulrich.com>
2022-01-28 15:58:02 -1000

    file permisions

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eo pipefail
-
+git update-index --chmod=+x ./.github/scripts/*
 xcodebuild -project iosApp.xcodeproj \
             -scheme iosApp \
             -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
